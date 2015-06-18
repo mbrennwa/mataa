3267e2be20195d368ce87da0a5f654bd974686e4
mbrennwa <mbrennwa@d009e6a2-0348-0410-826e-dc98ae27b543>
2015-06-18 18:01:36 +0000

    Added loopback calibration of impulse response measurement (2)

--- mataa_tools/mataa_measure_IR.m
+++ mataa_tools/mataa_measure_IR.m
@@ -1,6 +1,6 @@
-function [h,t,unit] = mataa_measure_IR (input_signal,fs,N,latency,cal);
+function [h,t,unit] = mataa_measure_IR (input_signal,fs,N,latency,loopback,cal);
 
-% function [h,t,unit] = mataa_measure_IR (input_signal,fs,N,latency,cal);
+% function [h,t,unit] = mataa_measure_IR (input_signal,fs,N,latency,loopback,cal);
 %
 % DESCRIPTION:
 % This function measures the impulse response h(t) of a system using sample rate fs. The sampling rate must be supported by the audio device and by the TestTone program. See also mataa_measure_signal_response. h(t) is determined from the deconvolution of the DUT's response and the original input signal.
@@ -9,17 +9,24 @@ function [h,t,unit] = mataa_measure_IR (input_signal,fs,N,latency,cal);
 % input_signal: input signal, vector of signal samples or name to file with sample data. Files must be in ASCII format and contain a one-column vector of the signal samples, where +1.0 is the maximum and -1.0 is the minimum value. The file should be in the 'test_signals' path. NOTE: it can't hurt to have some zeros padded to the beginning and the end of the input_signal. This helps to avoid that the DUT's response is cut off due to the latency of the audio hardware (and possibly the 'flight time'  of the sound from a loudspeaker to a microphone).
 % N (optional): the impulse response is measured N times and the mean response is calculated from these measurements. N = 1 is used by default.
 % latency: see mataa_measure_signal_response
-% cal (optional): calibration data (struct or string, see mataa_load_calibration and mataa_signal_calibrate)
+% loopback (optional): flag to control the behaviour of deconvolution of the DUT and REF channels. If loopback = 0, the DUT signal is not deconvolved from the REF signal (no loopback calibration). Otherwise, the DUT signal is deconvolved from the REF channel. The allocation of the DUT and REF channels is taken from mataa_settings('channel_DUT') and mataa_settings('channel_REF'). Default value (if not specified) is loopback = 0.
+% cal (optional): calibration data (struct or (cell-)string, see mataa_load_calibration and mataa_signal_calibrate)
 % 
 % OUTPUT:
-%
 % h: impulse response
 % t: time
 % unit: unit of data in h
 %
 % DISCLAIMER:
 % This file is part of MATAA.
-% 
+%
+% EXAMPLES:
+%
+% measure impulse response using a sweep test signal (without any data calibration):
+% > s = mataa_signal_generator ('sweep',44100,0.2,[50 20000]);	% create test signal (sine sweep from 50 to 20000 Hz, 0.2 s long, with 44.1 kHz sampling frequency
+% > [h,t] = mataa_measure_IR (s,44100,1,0.1);					% measure impulse response using test signal s, allowing for 0.1 s latency of sound in/out
+% > plot (t,h)													% plot result
+%
 % MATAA is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation; either version 2 of the License, or
@@ -40,6 +47,10 @@ function [h,t,unit] = mataa_measure_IR (input_signal,fs,N,latency,cal);
 
 if ~exist ('N','var')
 	N=1;
+end
+
+if ~exist ('loopback','var')
+	loopback = 0;
 end
 
 for i = 1:N
@@ -69,22 +80,31 @@ for i = 1:N
 	for j=1:nChan
 	    H(:,j) = OUT(:,j) ./ IN;
 	end
-		
-	dummy = ifft (H);
-	dummy = dummy (1:l,:); % the other half is redundant since the signal is real
 	
-	% h (i.e. dummy) should be purely real, but numerical artefacts may generate small
-	% shifts into the complex domain. Compensate for this:
+	if ~loopback % no loopback calibration
+		H = H(:,mataa_settings('channel_DUT')); % discard REF data
+	else % deconvolve DUT from REF data
+		H = H(:,mataa_settings('channel_DUT')) ./ H(:,mataa_settings('channel_REF')); % normalize DUT response by REF transfer function
+		
+		warning ("mataa_measure_IR: DUT/REF deconvolution needs proper testing! Be careful with results...")
+		
+	end
+	
+	dummy = ifft (H);	
+	dummy = dummy(1:l); % the other half is redundant since the signal is real
 	dummy = abs (dummy) .* sign (real(dummy)); % turn it back to the real-axis (complex part is much smaller than real part, so this works fine)
+
+	if ~loopback
+		dummy = -dummy; % get polarity right
+	end
+	
 	disp ('...deconvolution done.');
 	
-	dummy = dummy(:, mataa_settings ('channel_DUT')); % use DUT-data only
 	unit = char(unit{mataa_settings ('channel_DUT')});
-	
+		
 	if i == 1
 		h = dummy / N;
 	else
 		h = h + dummy / N;
 	end
-	h = -h; % get polarity right.
 end
