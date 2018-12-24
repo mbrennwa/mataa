b4863c95fd61d6fb8868c747ba6455774dfbd59d
mbrennwa <matthias@brennwald.org>
2018-12-24 11:27:46 +0100

    expanded acoustic script for different test signals and averaging feature

--- mataa_scripts/mataa_acoustic.m
+++ mataa_scripts/mataa_acoustic.m
@@ -28,28 +28,87 @@ if ~exist('fs','var')
 end
 disp (sprintf('Sampling rate fs = %g Hz',fs))
 
-% sine sweep:
-if ~exist('T','var')
-	T = input ('Sine sweep duration (s): ')
+% test signal
+if ~exist('sig','var')
+	sig = -1;
+	while isempty(find(sig==[1:3]))
+		sig = menu ('Choose test signal', {'Sine sweep','MLS','Pink noise'});
+	end
+	switch sig
+		case 1
+			sig = 'sweep';
+		case 2
+			sig = 'MLS';
+		case 3
+			sig = 'pinknoise';
+	end
 end
-disp (sprintf('Sine sweep duration T = %g s',T))
+disp (sprintf('Test signal sig = %s',sig))
 
-if ~exist('fL','var')
-	fL = input ('Sine sweep start frequency (Hz): ')
+switch (sig)
+
+	% sine sweep:
+	case 'sweep'
+		if ~exist('T','var')
+			T = input ('Sine sweep duration (s): ')
+		end
+
+		if ~exist('fL','var')
+			fL = input ('Sine sweep start frequency (Hz): ')
+		end
+		fL = max([1/T,fL]);
+		disp (sprintf('Sine sweep start frequency fL = %g Hz',fL))
+
+		if ~exist('fH','var')
+			fH = input ('Sine sweep end frequency (Hz): ')
+		end
+		fH = min([fs/2,fH]);
+		disp (sprintf('Sine sweep end frequency fH = %g Hz',fH))
+
+		s0 = mataa_signal_generator ('sweep',fs,T,[fL fH]); % log-sweep from fL to fH
+
+
+	% MLS
+	case 'MLS'
+		if ~exist('nMLS','var')
+			nMLS = input ('Number of MLS taps n (MLS length = 2^(n-1)): ');
+		end
+		s0 = mataa_signal_generator ('MLS',fs,0,nMLS); % MLS of length 2^(n-1)
+		fH = fL = [];
+		T = 2^(nMLS-1) / fs; % length of signal
+
+	% Pink noise:
+	case 'pinknoise'
+		if ~exist('T','var')
+			T = input ('Sine sweep duration (s): ')
+		end
+		s0 = mataa_signal_generator ('pink',fs,T); % pink noise sequence of length T
+		fH = fL = [];
 end
-fL = max([1/T,fL]);
-disp (sprintf('Sine sweep start frequency fL = %g Hz',fL))
 
-if ~exist('fH','var')
-	fH = input ('Sine sweep end frequency (Hz): ')
-end
-fH = min([fs/2,fH]);
-disp (sprintf('Sine sweep end frequency fH = %g Hz',fH))
+disp (sprintf('Test signal length T = %g s',T))
 
+% Set RMS value of test signal:
 if ~exist('U0rms','var')
-	U0rms = input ('Sine sweep amplitude (V-RMS): ')
+	U0rms = input ('Test signal amplitude (V-RMS): ');
 end
-disp (sprintf('Sine sweep amplitude U0rms = %g V-RMS',U0rms))
+disp (sprintf('Test signal amplitude U0rms = %g V-RMS',U0rms))
+s0 = s0 * U0rms/sqrt(sum(s0.^2)/length(s0)); % normalise to desired RMS level
+
+
+% Number of repeats for averaging:
+if ~exist('Nrepeat','var')
+	Nrepeat = input ('Number of repeats for averaging (leave empty for Nrepeat = 1): ');
+end
+if isempty (Nrepeat)
+	Nrepeat = 1;
+end
+Nrepeat = round(Nrepeat);
+if Nrepeat == 1
+	disp ('Singe measurement / no averaging, Nrepeat = 1.');
+else
+	disp (sprintf('Number of repeated measurements for averaging Nrepeat = %i',Nrepeat))
+end
 
 if ~exist('loopback','var')
 	x = input ('Do you want to use loopback compensation (Y/n)?','s');
@@ -81,19 +140,23 @@ end
 
 % cut-off / time gate:
 if ~exist('fc','var')
-	fc = input ('Enter lower cut-off frequency (--> gate time) (Hz): ')
+	fc = input ('Lower cut-off frequency for SPL display (--> gate time) (Hz): ');
 end
-disp (sprintf('Time-domain gating / cut-off frequency fc = %g Hz',fc))
+if isempty (fc)
+	disp ('Time gating turned off, fc = [].')
+else
+	disp (sprintf('Time-domain gating / cut-off frequency fc = %g Hz',fc))
+end
 
 % SPL smoothing / resolution:
 if ~exist('res','var')
-	res = input ('SPL response smoothing (octave-fraction, leave empty for no smoothing): ')
+	res = input ('SPL response smoothing (octave-fraction, leave empty for no smoothing): ');
 end
 if res <= 0
 	res = [];
 end
 if isempty(res)
-	disp ('No SPL response smoothing')
+	disp ('SPL response smoothing turned off, res = [].')
 else
 	disp (sprintf('SPL response smoothing 1/res = 1/%i octave',res))
 end
@@ -102,7 +165,7 @@ end
 if ~exist('col','var')
 	col = input ('Plot color (char: k, r, g, b, c, m, y): ','s');
 end
-if isempty ('col')
+if isempty(cell2mat(strfind(col,{'k', 'r', 'g', 'b', 'c', 'm', 'y'})))
 	col = 'r';
 end
 disp (sprintf('Plot color col = %s',col))
@@ -110,14 +173,10 @@ style = sprintf('%s-',col);
 
 
 % Ready? Input ok?
-input ('Ready to start? Press ENTER...')
- 
-% test signal:
-s0 = mataa_signal_generator ('sweep',fs,T,[fL fH]); % log-sweep from fL to fH
-s0 = s0 * U0rms/sqrt(sum(s0.^2)/length(s0)); % normalise to desired RMS level
+input ('Ready to start? Press ENTER...');
 
 % measure impulse response:
-[h,t,unit] = mataa_measure_IR (s0,fs,1,0.2,loopback,calfile,'V');
+[h,t,unit] = mataa_measure_IR (s0,fs,Nrepeat,0.2,loopback,calfile,'V');
 
 % time gating:
 [t_start,t_rise] = mataa_guess_IR_start (h,fs);
@@ -148,7 +207,7 @@ title ('SPL response')
 hold on
 
 % always save to "LastMeasurement.mat":
-save ('-V7','LastMeasurementIMUPLSERESPONSE.mat','h','t','unit','fL','fH','fs','T','calfile','loopback','U0rms');
+save ('-V7','LastMeasurementIMUPLSERESPONSE.mat','h','t','unit','fL','fH','fs','T','calfile','loopback','U0rms','sig');
 
 % Ask to save file:
 x = input ('Do you want to save raw data (impulse response) to a file (y/N)?','s');
@@ -159,7 +218,7 @@ if upper(x) == 'Y'
 	fn = uiputfile('*.mat','Choose file to save raw data...');
 	if ischar(fn)
 		info = input ('Enter data description: ','s')
-		save ('-V7',fn,'h','t','unit','fL','fH','fs','T','calfile','loopback','U0rms','info');
+		save ('-V7',fn,'h','t','unit','fL','fH','fs','T','calfile','loopback','U0rms','sig','info');
 		disp (sprintf('Saved impulse response data to file %s.',fn));
 	else
 		disp ('File not saved.')
